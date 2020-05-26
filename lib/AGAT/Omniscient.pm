e27856d588548539315a9cf1bd5ceffe5d90b309
Jacques Dainat <jacques.dainat@nbis.se>
2020-05-26 10:46:54 +0200

    increment AGAT version

--- lib/AGAT/Omniscient.pm
+++ lib/AGAT/Omniscient.pm
@@ -14,7 +14,7 @@ use AGAT::OmniscientStat;
 use AGAT::Utilities;
 use AGAT::PlotR;
 
-our $VERSION     = "v0.2.3";
+our $VERSION     = "v0.3.0";
 our @ISA         = qw(Exporter);
 our @EXPORT      = qw(get_agat_header);
 sub import {
