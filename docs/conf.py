dfcba808ae6d9739b83e0b3b81cc5383469d30b5
Jacques Dainat <jacques.dainat@nbis.se>
2021-04-14 21:23:50 +0200

    add extension like recommonmark

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,8 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+'sphinxemoji.sphinxemoji',
+'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
