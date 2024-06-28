c2ff6a4cea5faaf9bd92b7a85e80c19cb01f41a1
Jacques Dainat <jacques.dainat@ird.fr>
2024-06-28 16:39:10 +0200

    increment AGAT version

--- lib/AGAT/AGAT.pm
+++ lib/AGAT/AGAT.pm
@@ -16,7 +16,7 @@ use AGAT::Utilities;
 use AGAT::PlotR;
 use Bio::Tools::GFF;
 
-our $VERSION     = "v1.4.0";
+our $VERSION     = "v1.4.1";
 our @ISA         = qw(Exporter);
 our @EXPORT      = qw(get_agat_header print_agat_version get_agat_config handle_levels);
 sub import {
