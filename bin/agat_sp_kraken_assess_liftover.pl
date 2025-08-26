a4f0ca6d10abc28ed986ec54bae073b2953ecbb6
Kim Fehl <25846538+kim-fehl@users.noreply.github.com>
2025-08-26 23:39:16 +0400

    Update agat_sp_kraken_assess_liftover.pl

--- bin/agat_sp_kraken_assess_liftover.pl
+++ bin/agat_sp_kraken_assess_liftover.pl
@@ -44,7 +44,7 @@ my $log;
 if ( my $log_name = $config->{log_path} ) {
     open( $log, '>', $log_name ) or die "Can not open $log_name for printing: $!";
 }
-dual_print( $log, $header);
+dual_print( $log, $header, 3);
 
 sub _kraken_has_tag {
     my ($feature) = @_;
